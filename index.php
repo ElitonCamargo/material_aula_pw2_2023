d4f1daaecd345c8dbb33116841a9cb1dbd338f45
Eliton Camargo <camargoliveira@gmail.com>
2023-04-17 09:46:31 -0300

    Aula 2023-04-17

--- index.php
+++ index.php
@@ -1,49 +1,83 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula 27/02/2023</title>
+    <title>Document</title>
 </head>
 <body>
-
-    <form method="get">
-        <input type="text" name="txt_nome" placeholder="Nome">
+    <form method="post">
+        <input type="text" name="tab" placeholder="Tabuada">
         <br>
-        <input type="text" name="txt_sobre" placeholder="Sobrenome">
+        <input type="text" name="inicio" placeholder="Ínicio">
         <br>
-        <input type="submit" value="Enviar">
+        <input type="text" name="termino" placeholder="Término">
+        <br>
+        <input type="submit" name="btnCalc" value="Calcular">
     </form>
-
-    <!-- IMC = peso / (altura x altura) -->
-
     <?php
-        // -- Extraindo variável de variável
-        // $n = "Eliton";
-        // $nome = "n" ;
-        // $variavel = "nome";
-        // echo $$$variavel;
+        if(isset($_POST['btnCalc'])){
+            $tab = $_POST['tab'];
+            $inicio = $_POST['inicio'];
+            $termino = $_POST['termino'];
+            
+            $x = $inicio;
+            do{
+                $result = $tab * $x;
+                echo "$tab * $x = $result <br>";
+                $x++;
+            }while($x <= $termino);
 
-        var_dump($_GET);
+            // while($x <= $termino){
+            //     $result = $tab * $x;
+            //     echo "$tab * $x = $result <br>";
+            //     $x++;
+            // }
+            
+        }
 
-        $nome = @$_GET['txt_nome'];
-        $sobrenome = @$_GET['txt_sobre'];
-        echo "Nome completo: $nome $sobrenome";
 
-        $divisor = 10;
-        $dividendo = 1;
-        $quociente = $divisor/$dividendo;
-        $resto;
 
-        var_dump(
-            $divisor,
-            $dividendo,
-            $quociente,
-            @$resto
-        );
 
-       
+
+
+        // if(isset($_POST['tab'])){
+        //     $tab = $_POST['tab'];
+        //     $inicio = $_POST['inicio'];
+        //     $termino =  $_POST['termino'];
+        //     for($id = $inicio; $id <= $termino; $id++){
+        //         $result = $id * $tab;
+        //         echo "$tab * $id = $result<br>";
+        //     }            
+        // }
     ?>
 </body>
 </html>
+<?php
+// $valor = -1;
+// while($valor < 101){
+//     echo '<br> Valor = '.$valor;
+//     $valor += 1;
+// }
+
+// do{
+//     $valor += 1;
+//     echo '<br> Valor = '.$valor;
+// }while($valor < 100);
+
+// $x = 0;
+// for($id = 0; $id < 701; $id+=7)
+// {
+//     echo "7 * $x = {$id}<br>";
+//     $x +=1;
+// }
+
+// $tab = 5;
+// $inicio = 0;
+// $termino = 10;
+// for($id = $inicio; $id <= $termino; $id++){
+//     $result = $id * $tab;
+//     echo "$tab * $id = $result<br>";
+// }
+?>
