2a4dba19f15210f29d2a5b2e00b8b22c29882c5e
Eliton Camargo <camargoliveira@gmail.com>
2023-05-08 09:23:26 -0300

    Arrays

--- aula0508.php
+++ aula0508.php
@@ -1,1 +1,39 @@
 <?php
+
+// $produtos = array(
+//     "Amarelo",
+//     "Vermelho",
+//     "Branco"
+// );
+// $produtos[] = "Verde";
+// $produtos[] = "Preto";
+
+// var_dump($produtos);
+// $produtos = array_reverse($produtos);
+// var_dump($produtos);
+
+$produtos = [
+    'Amarelo'   =>'#0ff',
+    'Vermelho'  =>'#f00',
+    'Branco'    =>'#fff',
+    'Verde'     =>'#0f0',
+    'Preto'     =>'#000'
+];
+
+var_dump(array_reverse($produtos));
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <div style="background-color: red;"></div>
+        <h2></h2>
+    </div>
+</body>
+</html>
